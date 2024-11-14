484fb1bef5cf098947695b4bcae961e1f1ee487c
HarishDuraikannan <harish.durai2004@gmail.com>
2024-11-14 16:09:23 +0530

    commit1

---
+++ arth.py
@@ -0,0 +1,17 @@
+print("Takshashila Univercity")
+print("----------------------")
+a=int(input("enter the first no:"))
+b=int(input("enter the secound no:"))
+print("Result")
+print("------")
+
+c=a+b
+print("Addition:",c)
+c=a-b
+print("Subtrcsion:",c)
+c=a*b
+print("Multiplication:",c)
+c=a/b
+print("Divition:",c)
+c=a%b
+print("Modul:",c)
